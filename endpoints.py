4d7564e94abf2b3f299cb196c271e053e125bbc2
olegcybulin <olegpipulin@mail.ru>
2026-09-14 15:40:03 +0300

    added file endpoints

---
+++ endpoints.py
@@ -0,0 +1,5 @@
+BASE_URL = "https://qa-scooter.education-services.ru"
+
+CREATE_COURIER = f"{BASE_URL}/api/v1/courier"
+LOGIN_COURIER = f"{BASE_URL}/api/v1/courier/login"
+ORDERS = f"{BASE_URL}/api/v1/orders"
